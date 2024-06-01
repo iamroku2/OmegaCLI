a33e1ea7085095338894f8f59be062394a98ca7b
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-06-02 00:50:02 +0100

    Update download.py

--- bot/workers/downloaders/download.py
+++ bot/workers/downloaders/download.py
@@ -418,7 +418,7 @@ class Downloader:
                     )
                 )
 
-            progress = "\n\n‣ [{0}{1}] {2}%\n".format(
+            progress = "\n‣ [{0}{1}] {2}%\n".format(
                 "".join([fin_str for i in range(math.floor(download.progress / 10))]),
                 "".join(
                     [unfin_str for i in range(10 - math.floor(download.progress / 10))]
