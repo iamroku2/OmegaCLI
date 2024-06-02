247ef3be10c87d4b8b6f8584fd4096f0e6b9dc43
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-06-02 06:53:32 +0100

    Update bot_utils.py

--- bot/utils/bot_utils.py
+++ bot/utils/bot_utils.py
@@ -40,7 +40,7 @@ class Var_list:
     LAST_ENCD = []
     PAUSEFILE = []
     USER_MAN = []
-    GROUPENC = Var.GROUP_ID
+    GROUPENC = Conf.GROUP_ID
     VERSION2 = []
     U_CANCEL = []
     R_QUEUE = []
