b643c3e437ed144c1ce8df318c76873c7ad1dc42
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-06-02 06:33:54 +0100

    Update bot_utils.py

--- bot/utils/bot_utils.py
+++ bot/utils/bot_utils.py
@@ -131,7 +131,7 @@ def get_var(var):
     var_dict = dict()
     var_dict.update(
         {
-            "groupenc": -1002074327490,
+            "groupenc": C.GROUP_ID,
             "startup": STARTUP,
             "version2": VERSION2,
             "pausefile": PAUSEFILE,
