6f17d6a582c20125da305084b4f3aec62a98a28a
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-06-02 03:57:00 +0100

    Update transcode.py

--- bot/workers/auto/transcode.py
+++ bot/workers/auto/transcode.py
@@ -291,7 +291,7 @@ async def thing():
         out =  f"{_dir}/{bb}"
         #title, epi, sn, rlsgrp = await dynamicthumb(name, _filter=f)
 
-        c_n = f"{title} {sn or str()}".strip()
+        c_n = f"{sn or str()}".strip()
         if einfo.previous and einfo.previous == c_n:
             pass
         else:
