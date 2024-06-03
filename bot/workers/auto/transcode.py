f4e17205ed32d5c059400fbc08519526ab04d420
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-06-03 14:42:53 +0100

    Update transcode.py

--- bot/workers/auto/transcode.py
+++ bot/workers/auto/transcode.py
@@ -416,20 +416,10 @@ async def thing():
         per = str(f"{pe:.2f}") + "%"
         mux_msg = f"Muxed in `{mtime}`\n" if mux_args else str()
 
-        dk = f"<b>File Name:"
         text = str()
         mi = await info(dl)
         forward_task = asyncio.create_task(forward_(name, out, up, mi, f))
-        ds = await e.client.send_file(
-            e.chat_id, file=ok, force_document=True, caption=dk, link_preview=False, thumb=thum, parse_mode="html",
-            buttons=[
-                [Button.url("Before Info", url=mi), Button.url("After Info", url=mi)],
-                [Button.inline(f"⚡ Encoded 480p", data="stat11")],
-               # [Button.inline(f"Original File Size: {hbs(org)}", data="stat3")], 
-                #[Button.inline(f"Encoded File Size: {hbs(com)}", data="stat22")],
-                [Button.inline(f"Encoded in: {etime}", data="stat9")],
-                #[Button.inline(f"📥 {x}", data="statx"), Button.inline(f"📤 {xxx}", data="stat8")],
-            ],
+        
 
     #    text += f"**Source:** `[{rlsgrp}]`"
        # if mi:
@@ -439,17 +429,17 @@ async def thing():
         #    disable_web_page_preview=True,
          #   quote=True,
       #  )
-      #  await mi_msg.copy(chat_id=log_channel) if op else None
+        await mi_msg.copy(chat_id=log_channel) if op else None
 
-       # st_msg = await up.reply(
-          #  f"**Encode Stats:**\n\nOriginal Size: "
-         #   f"`{hbs(org_s)}`\nEncoded Size: `{hbs(out_s)}`\n"
+        st_msg = await up.reply(
+            f"**Encode Stats:**\n\nOriginal Size: "
+            f"`{hbs(org_s)}`\nEncoded Size: `{hbs(out_s)}`\n"
          #   f"Encoded Percentage: `{per}`\n\n"
           #  f"{'Cached' if einfo.cached_dl else 'Downloaded'} in `{dtime}`\n"
         #    f"Encoded in `{etime}`\n{mux_msg}Uploaded in `{utime}`",
-          #  disable_web_page_preview=True,
-          #  quote=True,
-       # )
+            disable_web_page_preview=True,
+            quote=True,
+        )
         await st_msg.copy(chat_id=log_channel) if op else None
         await forward_task
 
