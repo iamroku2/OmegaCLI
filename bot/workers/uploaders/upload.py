2088fc04f9a34bc6c9015bc24de576db2a21630d
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-06-04 02:39:15 +0100

    Update upload.py

--- bot/workers/uploaders/upload.py
+++ bot/workers/uploaders/upload.py
@@ -10,7 +10,7 @@ from bot.utils.bot_utils import UN_FINISHED_PROGRESS_STR as unfin_str
 from bot.utils.bot_utils import code, decode, hbs, time_formatter
 from bot.utils.log_utils import logger
 from bot.utils.os_utils import file_exists
-from bot.utils import *
+from .ani_utils import *
 
 from bot.utils.bot_utils import encode_info as einfo
 from bot.utils.bot_utils import get_bqueue, get_queue, get_var, hbs
