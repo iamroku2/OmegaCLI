3b7ddc203b8973a94ec43860262b55886105fd1e
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-06-06 15:27:35 +0100

    Update queue.py

--- bot/workers/handlers/queue.py
+++ bot/workers/handlers/queue.py
@@ -69,7 +69,7 @@ async def listqueue(event, args, client, deletable=True):
             return await listqueuep(event, or_args, client)
         return await event.reply(f"Unknown args: {args}")
     if deletable:
-        if not user_is_owner(event.sender_id) and event.is_private:
+         if not user_is_owner(event.sender_id) and event.is_private:
             return 
          if event.is_channel and not event.is_group:
             return
